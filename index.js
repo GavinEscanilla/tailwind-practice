d9d43658c40b31f8571bb0ad638475e3779d0dff
GavinEscanilla <gavinaltonescanilla@gmail.com>
2025-03-12 17:44:34 -0400

    about img section
    created a array of objects of different iems, then added a eventlistener for every click it will change the iem img and name, with making sure to cycle back to the first

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log([1,32,5,55,6,76].sort((a,b) => b-a));
+import { IEMs } from "./list.js";
+const IMGS = document.getElementById("about-img");
+const SRC = document.getElementById("img-src");
+const NAME = document.getElementById("img-name");
+
+let index =0;
+SRC.src = IEMs[index].img;
+NAME.innerHTML = IEMs[index].name;
+
+IMGS.addEventListener("click", () => {
+    index = (index + 1) % IEMs.length;
+    SRC.src = IEMs[index].img;
+    NAME.innerHTML = IEMs[index].name;
+})
